8739464c173f58d944e19d237e74b753ef0417ff
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-03-10 15:31:26 +0100

    Remove unused using statement from Program.cs.

--- WebSudoku.Shared.MeasureTime/Program.cs
+++ WebSudoku.Shared.MeasureTime/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using WebSudoku.Shared;
 using WebSudoku.Shared.MeasureTime;
 
 Console.WriteLine("Time Mesurement App for Websudoku.Shared project");
