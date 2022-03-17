634a0447cfaa15ca48c4a6054a04d6e221fbcb41
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-03-17 17:46:10 +0100

    Refactor Neighbors class.
    Mark some methods as static.

--- WebSudoku/Shared/Sudoku/Neighbors.cs
+++ WebSudoku/Shared/Sudoku/Neighbors.cs
@@ -7,29 +7,47 @@
         public Neighbors()
         {
             CellNeighbors = new HashSet<CellPosition>[9, 9];
+
+            InitializeCellNeighbors(CreateSquareCellsArray());
+        }
+
+        private void InitializeCellNeighbors(IEnumerable<CellPosition>[,] squareCells)
+        {
             for (int row = 0; row < 9; row++)
             {
                 for (int column = 0; column < 9; column++)
                 {
-                    CellNeighbors[row, column] = new HashSet<CellPosition>(WithinSquare(row, column)
-                        .Concat(WithinRow(row)
-                        .Concat(WithinColumn(column))));
+                    CellNeighbors[row, column] = new(WithinRow(row).Concat(WithinColumn(column)).Concat(squareCells[row / 3, column / 3]));
                 }
             }
         }
 
-        public IEnumerable<CellPosition> WithinSquare(int row, int column)
+        private static IEnumerable<CellPosition>[,] CreateSquareCellsArray()
         {
-            return Enumerable.Range(row / 3 * 3, 3)
-                .Join(Enumerable.Range(column / 3 * 3, 3), outer => 0, inner => 0, (r, c) => new CellPosition(r, c));
+            var squareCells = new IEnumerable<CellPosition>[3, 3];
+            for (int squareRow = 0; squareRow < 3; squareRow++)
+            {
+                for (int squareColumn = 0; squareColumn < 3; squareColumn++)
+                {
+                    squareCells[squareRow, squareColumn] = WithinSquare(squareRow, squareColumn);
+                }
+            }
+
+            return squareCells;
         }
 
-        public IEnumerable<CellPosition> WithinRow(int row)
+        public static IEnumerable<CellPosition> WithinSquare(int row, int column)
+        {
+            return Enumerable.Range(row * 3, 3)
+                .Join(Enumerable.Range(column * 3, 3), outer => 0, inner => 0, (r, c) => new CellPosition(r, c));
+        }
+
+        public static IEnumerable<CellPosition> WithinRow(int row)
         {
             return Enumerable.Range(0, 9).Select(column => new CellPosition(row, column));
         }
 
-        public IEnumerable<CellPosition> WithinColumn(int column)
+        public static IEnumerable<CellPosition> WithinColumn(int column)
         {
             return Enumerable.Range(0, 9).Select(row => new CellPosition(row, column));
         }
