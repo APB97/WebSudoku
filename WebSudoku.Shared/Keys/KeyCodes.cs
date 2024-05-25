48631eb3f973fd0f832adc77b8151691b85586d6
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-05-25 16:06:30 +0200

    Reorganize layout of Sudoku compnent.
    Replace setting background-color with setting border for sudoku-table cells.
    
    Add hide-in-print class to div.sidebar and div.top-row.
    
    Simplify CSS.
    
    Add KeyCodes class with values for arrow keys.
    
    Add OnKeyDown EventCallback.
    
    Add focus-sudoku-cell.js used for navigation with arrows.

---
+++ WebSudoku.Shared/Keys/KeyCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace WebSudoku.Shared.Keys
+{
+    public static class KeyCodes
+    {
+        public const string ArrowLeft = "ArrowLeft";
+        public const string ArrowRight = "ArrowRight";
+        public const string ArrowUp = "ArrowUp";
+        public const string ArrowDown = "ArrowDown";
+    }
+}
---
+++ WebSudoku/wwwroot/js/sudoku-cell-focus.js
@@ -0,0 +1,5 @@
+﻿function focusCell(row, column) {
+    let cell = document.querySelector(`table.sudoku-table > tr:nth-child(${row}) > td:nth-child(${column}) > input`);
+    cell.focus();
+    cell.select();
+}
