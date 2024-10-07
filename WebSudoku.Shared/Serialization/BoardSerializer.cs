246ca6357eb681263dbe700bd0c84c8c1d3bb9f5
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-07 18:34:49 +0200

    Update save and load to use single localStorage key-value pair
    Add Saving and loading section to Index
    
    Update loading logic keeping support for loading V1 save

--- WebSudoku.Shared/Serialization/BoardSerializer.cs
+++ WebSudoku.Shared/Serialization/BoardSerializer.cs
@@ -5,14 +5,26 @@ namespace WebSudoku.Shared.Serialization
 {
     public static class BoardSerializer
     {
-        public static Board? DeserializeFromJson(string json, Solver solver)
+        public static Board? DeserializeFromJson(string json)
         {
             return new Board(JsonSerializer.Deserialize<BoardState>(json));
+        }
+
+        public static Board DeserializeFromJson(string json, out TimeSpan? timer)
+        {
+            GameState gameState = JsonSerializer.Deserialize<GameState>(json);
+            timer = gameState.Timer;
+            return new Board(gameState.Board);
         }
 
         public static string SerializeToJson(Board board)
         {
             return JsonSerializer.Serialize(board.GetState());
+        }
+
+        public static string SerializeToJson(Board board, TimeSpan? timer, string version)
+        {
+            return JsonSerializer.Serialize(new GameState { Board = board.GetState(), Timer = timer, Version = version });
         }
     }
 }
---
+++ WebSudoku.Shared/Sudoku/GameState.cs
@@ -0,0 +1,3 @@
+﻿namespace WebSudoku.Shared.Sudoku;
+
+public readonly record struct GameState(string Version, BoardState Board, TimeSpan? Timer) { }
