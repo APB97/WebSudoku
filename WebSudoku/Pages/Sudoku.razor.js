4957c8968628681261e580c49a813ae1a7954f1a
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-11 15:45:47 +0200

    Move focusCell into Sudoku.razor.js module

--- WebSudoku/Pages/Sudoku.razor.js
+++ WebSudoku/Pages/Sudoku.razor.js
@@ -2,3 +2,9 @@
 {
     return /firefox/i.test(navigator.userAgent) && !/mobile/i.test(navigator.userAgent);
 }
+
+export function focusCell(row, column) {
+    let cell = document.querySelector(`table.sudoku-table > tr:nth-child(${row}) > td:nth-child(${column}) > input`);
+    cell.focus();
+    cell.select();
+}
