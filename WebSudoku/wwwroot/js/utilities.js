5b6087af36c41199fef00c784cdb755e6f2537b1
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-14 16:11:27 +0200

    Move getSetting and setSetting to utilities.js module

--- WebSudoku/wwwroot/js/utilities.js
+++ WebSudoku/wwwroot/js/utilities.js
@@ -1,3 +1,10 @@
 ﻿export function alert(message) {
     window.alert(message);
 }
+export function getSetting(key) {
+    return localStorage.getItem(key);
+}
+
+export function setSetting(key, value) {
+    localStorage.setItem(key, value);
+}
