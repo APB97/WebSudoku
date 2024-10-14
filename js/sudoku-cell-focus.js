52feed3fc13e5147bf4d617ae08a58f4345dfb52
APB97 <APB97@users.noreply.github.com>
2024-10-14 14:00:22 +0000

    deploy: 0cd0343fc0bed1f23b3856e9c77d0f745b928e9b

--- js/sudoku-cell-focus.js
+++
@@ -1,5 +0,0 @@
-﻿function focusCell(row, column) {
-    let cell = document.querySelector(`table.sudoku-table > tr:nth-child(${row}) > td:nth-child(${column}) > input`);
-    cell.focus();
-    cell.select();
-}
