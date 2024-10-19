5aa997e424bff763bbc1eb545081e4c1b14e7da0
APB97 <APB97@users.noreply.github.com>
2024-10-19 16:00:27 +0000

    deploy: 981d942d890854aba8b1ee14778bd76daefc9dad

--- js/utilities.js
+++ js/utilities.js
@@ -9,3 +9,15 @@ export function getSetting(key) {
 export function setSetting(key, value) {
     localStorage.setItem(key, value);
 }
+
+export function removeSetting(key) {
+    localStorage.removeItem(key);
+}
+
+export function getSessionSetting(key) {
+    return sessionStorage.getItem(key);
+}
+
+export function removeSessionSetting(key) {
+    sessionStorage.removeItem(key);
+}
