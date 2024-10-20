a19eff9f5e2774f4c7091ccca673d94a4fe2a70a
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-20 13:45:22 +0200

    Use CascadingParameter to replace usages of static components' members
    Add Hidden and HiddenForScreenWidth components along with ScreenBreakpoints and Window Dimensions classes

---
+++ WebSudoku/Services/Dimensions/ScreenBreakpoints.cs
@@ -0,0 +1,8 @@
+namespace apb97.github.io.WebSudoku.Services.Dimensions;
+
+public static class ScreenBreakpoints
+{
+    public const int Medium = 640; // px
+    public const int Large = 992; // px
+    public const int ExtraLarge = 1200; // px
+}
---
+++ WebSudoku/Services/Dimensions/WindowDimensions.cs
@@ -0,0 +1,8 @@
+﻿namespace apb97.github.io.WebSudoku.Services.Dimensions
+{
+    public class WindowDimensions
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
