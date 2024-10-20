4b7421ddd466176bdf66d709f8f003c8b346c1b3
APB97 <APB97@users.noreply.github.com>
2024-10-20 11:46:16 +0000

    deploy: a19eff9f5e2774f4c7091ccca673d94a4fe2a70a

---
+++ Shared/HiddenForScreenWidth.razor.js
@@ -0,0 +1,26 @@
+﻿export function addListener(dotnetHelper, methodName) {
+    window.addEventListener('load', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth);
+    });
+
+    window.addEventListener('resize', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth);
+    });
+}
+
+export function getDimensions() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
+
+export function removeListener() {
+    window.removeEventListener('load', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth)
+    });
+
+    window.removeEventListener('resize', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth)
+    })
+}
