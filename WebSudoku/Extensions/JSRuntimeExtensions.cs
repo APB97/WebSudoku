9ffe6432689ded7a65bf89801763ba1331925ac9
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-27 16:23:53 +0100

    Add Settings page
    Update translations

--- WebSudoku/Extensions/JSRuntimeExtensions.cs
+++ WebSudoku/Extensions/JSRuntimeExtensions.cs
@@ -6,8 +6,8 @@ public static class JSRuntimeExtensions
 {
     private const string ImportFunction = "import";
 
-    public static async ValueTask<IJSObjectReference> ImportAsync(this IJSRuntime js, string path)
+    public static ValueTask<IJSObjectReference> ImportAsync(this IJSRuntime js, string path)
     {
-        return await js.InvokeAsync<IJSObjectReference>(ImportFunction, path);
+        return js.InvokeAsync<IJSObjectReference>(ImportFunction, path);
     }
 }
